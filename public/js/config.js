318c09ecb6eff6827cf0624e9c87d5a71f4439f5
Nguyễn Hữu Thắng <84012140+nguyenhuuthang113@users.noreply.github.com>
2022-08-08 01:45:04 +0700

    ok

---
+++ public/js/config.js
@@ -0,0 +1,31 @@
+const WEB = {
+    title: "THÔNG TIN THANH TOÁN",
+    favicon: "logoweb.png",
+};
+
+const USER = {
+    username: "Việt Nam Bếp",
+    blueTick: false,
+    avatar: "a.jpg",
+    skills: [
+        "DESINGER",
+        "DEVELOPER"
+    ],
+    banks: [
+        {
+            name: "MB BANK",
+            logo: "mbbank1.png",
+            number: "585868686",
+            owner: "CONG TY TNHH VIET NAM BEP"
+        },
+        
+        {
+            name: "MOMO",
+            logo: "momo.png",
+            number: "0967352113",
+            owner: "Phạm Thị An Na",
+            fastTransfer: true
+        },
+        
+    ]
+};
